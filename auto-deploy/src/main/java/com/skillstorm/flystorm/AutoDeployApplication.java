4aeb2a8d215ee3cb0a7b896766408df166ee47bb
seancarter99 <scarter@skillstorm.com>
2023-05-11 23:45:45 -0400

    Adding Spring code

---
+++ auto-deploy/src/main/java/com/skillstorm/flystorm/AutoDeployApplication.java
@@ -0,0 +1,13 @@
+package com.skillstorm.flystorm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoDeployApplication.class, args);
+	}
+
+}
